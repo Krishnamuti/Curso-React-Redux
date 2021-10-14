b0e337b9872bc282e1d2abb5af686cd84e4daec2
Krishnamuti <61639711+Krishnamuti@users.noreply.github.com>
2021-10-14 19:40:35 -0300

    criando o projeto

---
+++ redux-simples/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import './Card.css'
+import React from 'react'
+
+export default props => {
+    return(
+        <div className="Card">
+            <div className="Header">
+                <span className="Title">{props.title}</span>
+            </div>
+            <div className="Content">
+                {props.children}
+            </div>
+        </div>
+    )
+}
