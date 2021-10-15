80f2456825179c3a8ef5556d772ab85dac1362ed
Krishnamuti <61639711+Krishnamuti@users.noreply.github.com>
2021-10-14 21:52:20 -0300

    componente Card

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -7,10 +7,12 @@ function App() {
     <div className="App">
       <h1>React-Redux (Simples)</h1>
       <div className="linha">
-        <Card>X</Card>
+        <Card title="Card 1" red>X</Card>
+        <Card title="Card 2" green>X</Card>
       </div>
       <div className="linha">
-        <Card>Y</Card>
+        <Card title="Card 3" blue>Y</Card>
+        <Card title="Card 4" purple>X</Card>
       </div>      
     </div>
   );
