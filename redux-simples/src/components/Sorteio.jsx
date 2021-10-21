d64fa5b04c19c41eb6446b810dbf9b0ad878246a
Krishnamuti <61639711+Krishnamuti@users.noreply.github.com>
2021-10-20 22:46:24 -0300

    conectando componentes com redux

--- redux-simples/src/components/Sorteio.jsx
+++ redux-simples/src/components/Sorteio.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Card from './Card'
 
-export default props => {
+function Sorteio(props) {
+    const { min, max } = props;
+    const aleatorio = parseInt(Math.random() * (max - min)) + min
     return (
         <Card title="Sorteio de um Número" purple>
             <div>
                 <span>Resultado:</span>
-                <strong>8</strong>
+                <strong>{aleatorio}</strong>
             </div>
         </Card>
     )
 }
+
+function mapStateToProps(state) {
+    return{
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Sorteio)
